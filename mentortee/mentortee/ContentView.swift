762bfdae21e0e0711e133c96e4930596fc110e56
태깅 <min4644@gmail.com>
2022-04-05 12:24:41 +0900

    TabView 생성

---
+++ mentortee/mentortee/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  mentortee
+//
+//  Created by 김민택 on 2022/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+                Text("home").tabItem { Image(systemName: "house").environment(\.symbolVariants, .none) }.tag(1)
+                Text("feed").tabItem { Image(systemName: "square.text.square").environment(\.symbolVariants, .none) }.tag(2)
+                Text("Tab Content 3").tabItem { Image(systemName: "doc.text").environment(\.symbolVariants, .none) }.tag(3)
+                Text("Tab Content 4").tabItem { Image(systemName: "person").environment(\.symbolVariants, .none) }.tag(4)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mentortee/mentortee/mentorteeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mentorteeApp.swift
+//  mentortee
+//
+//  Created by 김민택 on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct mentorteeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
