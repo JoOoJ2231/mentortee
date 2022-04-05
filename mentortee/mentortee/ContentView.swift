da63e49fa6dfdd24fb006d38bafa0277657d68ed
태깅 <min4644@gmail.com>
2022-04-05 12:28:05 +0900

    TabView 수정

--- mentortee/mentortee/ContentView.swift
+++ mentortee/mentortee/ContentView.swift
@@ -13,8 +13,8 @@ struct ContentView: View {
             TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
                 Text("home").tabItem { Image(systemName: "house").environment(\.symbolVariants, .none) }.tag(1)
                 Text("feed").tabItem { Image(systemName: "square.text.square").environment(\.symbolVariants, .none) }.tag(2)
-                Text("Tab Content 3").tabItem { Image(systemName: "doc.text").environment(\.symbolVariants, .none) }.tag(3)
-                Text("Tab Content 4").tabItem { Image(systemName: "person").environment(\.symbolVariants, .none) }.tag(4)
+                Text("my qna").tabItem { Image(systemName: "doc.text").environment(\.symbolVariants, .none) }.tag(3)
+                Text("my page").tabItem { Image(systemName: "person").environment(\.symbolVariants, .none) }.tag(4)
             }
         }
     }
