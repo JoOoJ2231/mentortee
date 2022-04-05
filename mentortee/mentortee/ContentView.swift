1419ad3f14279bda38c84fd26f8591488cf60ff7
MMMIIIN <wbsldj7645@naver.com>
2022-04-05 22:48:10 +0900

    MainPage 연결

--- mentortee/mentortee/ContentView.swift
+++ mentortee/mentortee/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
-                Text("home").tabItem { Image(systemName: "house").environment(\.symbolVariants, .none) }.tag(1)
+                MainPage().tabItem { Image(systemName: "house").environment(\.symbolVariants, .none) }.tag(1)
                 Text("feed").tabItem { Image(systemName: "square.text.square").environment(\.symbolVariants, .none) }.tag(2)
                 Text("my qna").tabItem { Image(systemName: "doc.text").environment(\.symbolVariants, .none) }.tag(3)
                 Text("my page").tabItem { Image(systemName: "person").environment(\.symbolVariants, .none) }.tag(4)
