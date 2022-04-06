f01beb4a234d5c9ebc1db0acfe69423e2b575ce8
MMMIIIN <wbsldj7645@naver.com>
2022-04-06 10:47:13 +0900

    폴더 재정리
    제발

---
+++ mentortee/mentortee/Views/MyPageView/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  mentortee
+//
+//  Created by Mingwan Choi on 2022/04/06.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
